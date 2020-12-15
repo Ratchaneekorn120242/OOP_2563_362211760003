ec096ac56563faa9691db3299621194d4ddc34d0
Ratchaneekorn120242 <75650639+Ratchaneekorn120242@users.noreply.github.com>
2020-12-15 08:48:53 +0700

    Update HolloWorld.java

--- src/LAB1/HolloWorld.java
+++ src/LAB1/HolloWorld.java
@@ -5,7 +5,8 @@ public class HolloWorld {
     public static void main(String[] args) {
 
       System.out.println("Hello World");
-
+        System.out.println("Hello World");
+         System.out.println("Hello World");
     }//main
 
 
